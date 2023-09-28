89f65e58dc5593e147428e801351045a5d9d3e37
REMEESH <remeesh97@gmail.com>
2023-09-28 13:32:20 +0530

    Media Player

---
+++ OneDrive/Desktop/Luminar/REACT/media player/veedio-player/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Home from './Pages/Home';
+import LandingPage from './Pages/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Media Player App </h1>
+      <LandingPage/>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
