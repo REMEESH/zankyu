9058e7b7b94825f96dde6865368bd39f138ede09
REMEESH <remeesh97@gmail.com>
2023-10-26 11:13:59 +0530

    project-fair

---
+++ OneDrive/Desktop/Luminar/MERN/project-fair/src/App.js
@@ -0,0 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import Footer from './components/Footer';
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/register'  element={<Register/>} />
+        <Route path='/projects' element={<Projects/>} />
+        <Route path='/dashboard' element={<Dashboard/>} />
+
+      </Routes>
+      <Footer/>
+     
+    </>
+  );
+}
+
+export default App;
